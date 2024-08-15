d681b972a8851d9b291c3c507457a9156b3aa8c1
Gabriel Pivovarov <176077153+gsharpsharp@users.noreply.github.com>
2024-08-15 07:56:34 -0700

    Add PostgreSQL full text search functionality

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.contrib.postgres.search import (
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+)
 from django.views import generic
 
 from shop.models import Product, ProductCategory
@@ -8,7 +14,19 @@ class ProductListView(generic.ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        products = Product.objects.all()
+        if 'search' in self.request.GET and self.request.GET['search'] != '':
+            vector = (
+                SearchVector('name', weight='A')
+                + SearchVector('category_names', weight='A')
+                + SearchVector('description', weight='D')
+            )
+            query = SearchQuery(self.request.GET['search'])
+            products = Product.objects.annotate(
+                category_names=ArrayAgg('categories__name', distinct=True),
+                rank=SearchRank(vector, query),
+            ).filter(rank__gte=0.1).order_by('-rank')
+        else:
+            products = Product.objects.all()
         if 'category' in self.request.GET:
             products = products.filter(
                 categories__slug=self.request.GET['category'],
